f926ab0aeb82ccb77e9218076245faa2e4f69b52
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-25 15:51:24 -0700

    put card information in allCards.js and pull into the html with card.js

--- js/card.js
+++ js/card.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var renderCard = function(card) {
+  var title = document.getElementById('title');
+  title.textContent = card.title;
+
+  var explanation = document.getElementById('explanation');
+  explanation.textContent = card.explanation;
+
+  var example = document.getElementById('example');
+  example.innerHTML = card.example;
+
+  var question = document.getElementById('question');
+  question.textContent = card.question;
+
+  var problem = document.getElementById('problem');
+  problem.innerHTML = card.problem;
+};
+
+renderCard(allCards[0]);
