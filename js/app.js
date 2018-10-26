4d892f35bee1ed2ba700b98d279fce27a05ad9ca
Richard Bellamy <richard.bellamy123@gmail.com>
2018-10-25 19:51:09 -0700

    quick fix

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
 'use strict';
-var subList = document.getElementById('itemList');
-//
-var subRenderFunc = function () {
-  subList = document.getElementById('itemList');
-  for (var i in allCards) {
-    var subButt = document.createElement('li');
-    subButt.setAttribute('id', i);
-    subButt.textContent = allCards[i].title;
-    subList.appendChild(subButt);
-  }
-};
-subRenderFunc();
-
 
