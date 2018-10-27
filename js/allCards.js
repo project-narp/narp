2ad0dd8d2cfa486cd8b74b6183d35d6f2471157d
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-27 13:40:44 -0700

    form working

--- js/allCards.js
+++ js/allCards.js
@@ -72,23 +72,22 @@ var allCards = [
   {
     title: 'Test',
     explanation: 'Text explanation',
-    example: `
-    function $initHighlight(block, cls) {
-      try {
-        if (cls.search(/\bno\-highlight\b/) != -1)
-          return process(block, true, 0x0F) +
-                \` class="\${cls}"\`;
-      } catch (e) {
-        /* handle exception */
-      }
-      for (var i = 0 / 2; i < classes.length; i++) {
-        if (checkCondition(classes[i]) === undefined)
-          console.log('undefined');
-      }
-    }
-    
-    export  $initHighlight;
-    `,
+    example: 
+`function $initHighlight(block, cls) {
+  try {
+    if (cls.search(/\bno\-highlight\b/) != -1)
+      return process(block, true, 0x0F) +
+            \` class="\${cls}"\`;
+  } catch (e) {
+    /* handle exception */
+  }
+  for (var i = 0 / 2; i < classes.length; i++) {
+    if (checkCondition(classes[i]) === undefined)
+      console.log('undefined');
+  }
+}
+
+export  $initHighlight;`,
     question: 'Test question',
     problem: `
     function $initHighlight(block, cls) {
