8e4338b8a342e2d4fa9b0cde2396afef42f7890a
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-28 12:54:08 -0700

    change form to navigate to the newly created card immediately after it is created rather than back to sub.html

--- js/newCard.js
+++ js/newCard.js
@@ -41,8 +41,10 @@ var newCardHandler = function(event){
   allCards.push(card);
   localStorage.setItem('allCards', JSON.stringify(allCards));
 
-  // navigate to concepts page after new card created
-  window.location.href = 'sub.html';
+  // navigate to new card page (not sub.html anymore) after new card created
+  // window.location.href = 'sub.html';
+  localStorage.setItem('cardIndex', allCards.length - 1);
+  window.location.href = 'card.html';
 };
 
 cardForm.addEventListener('submit', newCardHandler);
