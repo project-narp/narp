7cffcd686260266d31f282c00e3f5d3ae6b89cb0
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-29 12:35:01 -0700

    initial grid layout on sub page

--- js/newCard.js
+++ js/newCard.js
@@ -43,10 +43,10 @@ var newCardHandler = function(event){
   allCards.push(card);
   localStorage.setItem('allCards', JSON.stringify(allCards));
 
-  // navigate to new card page (not sub.html anymore) after new card created
-  // window.location.href = 'sub.html';
-  localStorage.setItem('cardIndex', allCards.length - 1);
-  window.location.href = 'card.html';
+  // navigate to sub.html (not newCard.html anymore) after new card created
+  window.location.href = 'sub.html';
+  // localStorage.setItem('cardIndex', allCards.length - 1);
+  // window.location.href = 'card.html';
 };
 
 cardForm.addEventListener('submit', newCardHandler);
--- js/sub.js
+++ js/sub.js
@@ -1,37 +1,61 @@
 'use strict';
 var subList2 = document.getElementById('main');
-// Andrews funtion to store to LS
+// Andrews code to store to / retrieve from localStorage
 if (!localStorage.getItem('allCards')){
   localStorage.setItem('allCards', JSON.stringify(allCards));
 } else {
   allCards = JSON.parse(localStorage.getItem('allCards'));
 }
-//make buttons when page is loaded
-var subButtRenderFunc = function(){
-  for (var i in allCards) {
-    var butt = document.createElement('button');
-    butt.setAttribute('type', 'button');
-    butt.setAttribute('id', i);
-    butt.textContent = allCards[i].title;
-    if(allCards[i].solved){
-      butt.setAttribute('class', 'solved');
+// //make buttons when page is loaded
+// var subButtRenderFunc = function(){
+//   for (var i in allCards) {
+//     var butt = document.createElement('button');
+//     butt.setAttribute('type', 'button');
+//     butt.setAttribute('id', i);
+//     butt.textContent = allCards[i].title;
+//     if(allCards[i].solved){
+//       butt.setAttribute('class', 'solved');
+//     }
+//     subList2.appendChild(butt);
+//   }
+//   butt = document.createElement('button');
+//   butt.setAttribute('type', 'button');
+//   butt.setAttribute('id', 'add');
+//   butt.textContent = '+';
+//   subList2.appendChild(butt);
+// };
+// subButtRenderFunc();
+// //do something when you click a button
+// var clickHandFunc = function(event){
+//   console.log(event.target);
+//   if(event.target.id === 'add'){
+//     window.location.href = 'newCard.html';
+//   } else if(event.target.type){
+//     localStorage.setItem('cardIndex', event.target.id);
+//     window.location.href = 'card.html';}
+// };
+// subList2.addEventListener('click', clickHandFunc);
+
+var renderCards = function() {
+  var fundamentals = document.getElementById('fundamentals');
+  var controlFlow = document.getElementById('control-flow');
+  var functions = document.getElementById('functions');
+  var dataStructures = document.getElementById('data-structures');
+  for (var card of allCards) {
+    var div = document.createElement('div');
+    var h3 = document.createElement('h2');
+    h3.textContent = card.title;
+    div.appendChild(h3);
+    if (card.group === 0) {
+      fundamentals.appendChild(div);
+    } else if (card.group === 1) {
+      controlFlow.appendChild(div);
+    } else if (card.group === 2) {
+      functions.appendChild(div);
+    } else {
+      dataStructures.appendChild(div);
     }
-    subList2.appendChild(butt);
   }
-  butt = document.createElement('button');
-  butt.setAttribute('type', 'button');
-  butt.setAttribute('id', 'add');
-  butt.textContent = '+';
-  subList2.appendChild(butt);
 };
-subButtRenderFunc();
-//do something when you click a button
-var clickHandFunc = function(event){
-  console.log(event.target);
-  if(event.target.id === 'add'){
-    window.location.href = 'newCard.html';
-  } else if(event.target.type){
-    localStorage.setItem('cardIndex', event.target.id);
-    window.location.href = 'card.html';}
-};
-subList2.addEventListener('click', clickHandFunc);
+
+renderCards();
