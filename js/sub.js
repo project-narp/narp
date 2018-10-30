69cc37414d7b2dfa898e5e18f53f5e49bc9c3106
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-29 21:39:22 -0700

    fix cardIndex resolving to 0 bug

--- js/sub.js
+++ js/sub.js
@@ -6,42 +6,17 @@ if (!localStorage.getItem('allCards')){
 } else {
   allCards = JSON.parse(localStorage.getItem('allCards'));
 }
-// //make buttons when page is loaded
-// var subButtRenderFunc = function(){
-//   for (var i in allCards) {
-//     var butt = document.createElement('button');
-//     butt.setAttribute('type', 'button');
-//     butt.setAttribute('id', i);
-//     butt.textContent = allCards[i].title;
-//     if(allCards[i].solved){
-//       butt.setAttribute('class', 'solved');
-//     }
-//     subList2.appendChild(butt);
-//   }
-//   butt = document.createElement('button');
-//   butt.setAttribute('type', 'button');
-//   butt.setAttribute('id', 'add');
-//   butt.textContent = '+';
-//   subList2.appendChild(butt);
-// };
-// subButtRenderFunc();
-// //do something when you click a button
-// var clickHandFunc = function(event){
-//   console.log(event.target);
-//   if(event.target.id === 'add'){
-//     window.location.href = 'newCard.html';
-//   } else if(event.target.type){
-//     localStorage.setItem('cardIndex', event.target.id);
-//     window.location.href = 'card.html';}
-// };
-// subList2.addEventListener('click', clickHandFunc);
 
 
 // ===========================================
 // HANDLERS
 // ===========================================
 var cardHandler = function(event) {
-  localStorage.setItem('cardIndex', Number(event.target.id));
+  // check for id, if not one get it from parent element
+  if (!event.target.id) {
+    var id = Number(event.target.parentElement.id);
+  }
+  localStorage.setItem('cardIndex', Number(event.target.id || id));
   window.location.href = 'card.html';
 };
 
